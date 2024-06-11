962c0042470da35706ebacb97dd0df7d4a615aa1
OsamaAmireh02 <osamamohdameerah@gmail.com>
2024-06-11 06:03:02 +0300

    First-Commit

--- src/Homework.java
+++ src/Homework.java
@@ -6,142 +6,22 @@ public class Homework extends SuperKarel {
     private int width = 0;
     private int height = 0;
 
+    public int calculate(){
+        int counter = 1;
+        while (frontIsClear()){
+            moveWithCalculation();
+            counter++;
+        }
+        turnLeft();
+        return counter;
+    }
+
     private void setWidth(){
         width = calculate();
     }
 
     private void setHeight(){
         height = calculate();
-    }
-
-    public void run() {
-        steps = 0;
-        setWidth();
-        setHeight();
-        setBeepersInBag(1000);
-
-        if (height == 2 && width == 2) {
-            put();
-            moveWithCalculation();
-            turnLeft();
-            moveWithCalculation();
-            put();
-            turnLeft();
-        } else if (width == 1) {
-            if (height == 2) {
-                turnLeft();
-                moveWithCalculation();
-            }
-            turnLeft();
-            if(height == 3){
-                moveWithCalculation();
-                put();
-                moveWithCalculation();
-                turnLeft();
-            }else if(height == 5) {
-                oneWithFiveCase();
-            }else if (height >= 4) {
-                oneWithMoreThanFourCases(height);
-            }
-        } else if(height == 1){
-            if (width == 2) {
-                moveWithCalculation();
-                turnAround();
-            }else if ( width == 3 ) {
-                moveWithCalculation();
-                put();
-                moveWithCalculation();
-                turnAround();
-            }else if(width == 5) {
-                oneWithFiveCase();
-            } else if (width >= 4) {
-                oneWithMoreThanFourCases(width);
-            }
-        } else if(width == 2){
-            if (height == 3) {
-                for (int i = 0; i < 2; i++) {
-                    put();
-                    moveWithCalculation();
-                    turnLeft();
-                    moveWithCalculation();
-                }
-                put();
-                turnAround();
-                moveWithCalculation();
-                turnAround();
-            } else if (height == 4) {
-                for (int i = 0; i < 2; i++) {
-                    moveWithCalculation();
-                    turnLeft();
-                    moveWithCalculation();
-                    put();
-                }
-            } else if (height == 5) {
-                moveWithCalculation();
-                turnLeft();
-                moveWithCalculation();
-                put();
-                turnLeft();
-                moveWithCalculation();
-                put();
-                turnRight();
-                moveWithCalculation();
-                moveWithCalculation();
-                put();
-                turnRight();
-                moveWithCalculation();
-                put();
-            } else if (height > 5) {
-                put();
-                moveWithCalculation();
-                put();
-                turnAround();
-                moveWithCalculation();
-                turnRight();
-                twoWithMoreThanFiveCases(height);
-            }
-        } else if (height == 2){
-            if (width == 3) {
-                moveWithCalculation();
-                put();
-                moveWithCalculation();
-                turnLeft();
-                moveWithCalculation();
-                turnLeft();
-                put();
-                moveWithCalculation();
-            } else if (width == 5) {
-                turnLeft();
-                moveWithCalculation();
-                turnRight();
-                moveWithCalculation();
-                putDouble();
-                moveWithCalculation();
-                moveWithCalculation();
-                putDouble();
-                moveWithCalculation();
-                turnAround();
-            } else if (width > 5) {
-                put();
-                turnLeft();
-                moveWithCalculation();
-                put();
-                turnRight();
-                twoWithMoreThanFiveCases(width);
-            }
-        } else if (width % 2 == 1 && height % 2 == 1) {
-            drawHorizontalWhenOdd();
-            drawVerticalWhenOdd();
-        } else if (width % 2 == 0 && height % 2 == 0) {
-            drawHorizontalWhenEven();
-            drawVerticalWhenEven();
-        } else if (width % 2 == 1 && height % 2 == 0) {
-            drawHorizontalWhenOdd();
-            drawVerticalWhenEven();
-        } else if (width % 2 == 0 && height % 2 == 1) {
-            drawHorizontalWhenEven();
-            drawVerticalWhenOdd();
-        }
     }
 
     public void put(){
@@ -153,16 +33,6 @@ public class Homework extends SuperKarel {
     public void moveWithCalculation() {
         move();
         System.out.println(++steps);
-    }
-
-    public int calculate(){
-        int counter = 1;
-        while (frontIsClear()){
-            moveWithCalculation();
-            counter++;
-        }
-        turnLeft();
-        return counter;
     }
 
     public void drawLine(){
@@ -296,5 +166,135 @@ public class Homework extends SuperKarel {
         turnAround();
         moveWithCalculation();
         turnRight();
+    }
+
+    public void run() {
+        steps = 0;
+        setWidth();
+        setHeight();
+        setBeepersInBag(1000);
+
+        if (height == 2 && width == 2) {
+            put();
+            moveWithCalculation();
+            turnLeft();
+            moveWithCalculation();
+            put();
+            turnLeft();
+        } else if (width == 1) {
+            if (height == 2) {
+                turnLeft();
+                moveWithCalculation();
+            }
+            turnLeft();
+            if(height == 3){
+                moveWithCalculation();
+                put();
+                moveWithCalculation();
+                turnLeft();
+            }else if(height == 5) {
+                oneWithFiveCase();
+            }else if (height >= 4) {
+                oneWithMoreThanFourCases(height);
+            }
+        } else if(height == 1){
+            if (width == 2) {
+                moveWithCalculation();
+                turnAround();
+            }else if ( width == 3 ) {
+                moveWithCalculation();
+                put();
+                moveWithCalculation();
+                turnAround();
+            }else if(width == 5) {
+                oneWithFiveCase();
+            } else if (width >= 4) {
+                oneWithMoreThanFourCases(width);
+            }
+        } else if(width == 2){
+            if (height == 3) {
+                for (int i = 0; i < 2; i++) {
+                    put();
+                    moveWithCalculation();
+                    turnLeft();
+                    moveWithCalculation();
+                }
+                put();
+                turnAround();
+                moveWithCalculation();
+                turnAround();
+            } else if (height == 4) {
+                for (int i = 0; i < 2; i++) {
+                    moveWithCalculation();
+                    turnLeft();
+                    moveWithCalculation();
+                    put();
+                }
+            } else if (height == 5) {
+                moveWithCalculation();
+                turnLeft();
+                moveWithCalculation();
+                put();
+                turnLeft();
+                moveWithCalculation();
+                put();
+                turnRight();
+                moveWithCalculation();
+                moveWithCalculation();
+                put();
+                turnRight();
+                moveWithCalculation();
+                put();
+            } else if (height > 5) {
+                put();
+                moveWithCalculation();
+                put();
+                turnAround();
+                moveWithCalculation();
+                turnRight();
+                twoWithMoreThanFiveCases(height);
+            }
+        } else if (height == 2){
+            if (width == 3) {
+                moveWithCalculation();
+                put();
+                moveWithCalculation();
+                turnLeft();
+                moveWithCalculation();
+                turnLeft();
+                put();
+                moveWithCalculation();
+            } else if (width == 5) {
+                turnLeft();
+                moveWithCalculation();
+                turnRight();
+                moveWithCalculation();
+                putDouble();
+                moveWithCalculation();
+                moveWithCalculation();
+                putDouble();
+                moveWithCalculation();
+                turnAround();
+            } else if (width > 5) {
+                put();
+                turnLeft();
+                moveWithCalculation();
+                put();
+                turnRight();
+                twoWithMoreThanFiveCases(width);
+            }
+        } else if (width % 2 == 1 && height % 2 == 1) {
+            drawHorizontalWhenOdd();
+            drawVerticalWhenOdd();
+        } else if (width % 2 == 0 && height % 2 == 0) {
+            drawHorizontalWhenEven();
+            drawVerticalWhenEven();
+        } else if (width % 2 == 1 && height % 2 == 0) {
+            drawHorizontalWhenOdd();
+            drawVerticalWhenEven();
+        } else if (width % 2 == 0 && height % 2 == 1) {
+            drawHorizontalWhenEven();
+            drawVerticalWhenOdd();
+        }
     }
 }
